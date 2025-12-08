efccf46c709a7ec8f8eeee4222b79fb3b61fa9d5
Wine93 <wine93.info@gmail.com>
2025-12-08 15:17:47 +0800

    ttt

--- src/cache/remotecache/prefetch.cc
+++ src/cache/remotecache/prefetch.cc
@@ -24,18 +24,185 @@
 
 #include <bthread/execution_queue.h>
 #include <bthread/execution_queue_inl.h>
+#include <bthread/mutex.h>
 #include <glog/logging.h>
+#include <sys/types.h>
 
 #include <atomic>
 #include <cstdio>
 #include <memory>
+#include <mutex>
+#include <utility>
 
 #include "cache/blockcache/cache_store.h"
 #include "cache/remotecache/prefetcher.h"
+#include "cache/utils/bthread.h"
+#include "common/io_buffer.h"
 #include "common/status.h"
 
 namespace dingofs {
 namespace cache {
+
+struct Handle {
+  void* value;
+  void (*deleter)(const std::string_view&, void* value);
+  Handle* next_hash;
+  Handle* next;
+  Handle* prev;
+  size_t charge;  // TODO(opt): Only allow uint32_t?
+  size_t key_length;
+  bool in_cache;     // Whether entry is in the cache.
+  uint32_t refs;     // References, including cache reference, if present.
+  uint32_t hash;     // Hash of key(); used for fast sharding and comparisons
+  char key_data[1];  // Beginning of key
+
+  std::string_view key() const {
+    // next is only equal to this if the LRU handle is the list head of an
+    // empty list. List heads never have meaningful keys.
+    assert(next != this);
+
+    return std::string_view(key_data, key_length);
+  }
+};
+
+LRUCache::LRUCache() : capacity_(0), usage_(0) {
+  // Make empty circular linked lists.
+  lru_.next = &lru_;
+  lru_.prev = &lru_;
+  in_use_.next = &in_use_;
+  in_use_.prev = &in_use_;
+}
+
+LRUCache::~LRUCache() {
+  assert(in_use_.next == &in_use_);  // Error if caller has an unreleased handle
+  for (LRUHandle* e = lru_.next; e != &lru_;) {
+    LRUHandle* next = e->next;
+    assert(e->in_cache);
+    e->in_cache = false;
+    assert(e->refs == 1);  // Invariant of lru_ list.
+    Unref(e);
+    e = next;
+  }
+}
+
+void LRUCache::Ref(LRUHandle* e) {
+  if (e->refs == 1 && e->in_cache) {  // If on lru_ list, move to in_use_ list.
+    LRURemove(e);
+    LRUAppend(&in_use_, e);
+  }
+  e->refs++;
+}
+
+void LRUCache::Unref(LRUHandle* e) {
+  assert(e->refs > 0);
+  e->refs--;
+  if (e->refs == 0) {  // Deallocate.
+    assert(!e->in_cache);
+    (*e->deleter)(e->key(), e->value);
+    free(e);
+  } else if (e->in_cache && e->refs == 1) {
+    // No longer in use; move to lru_ list.
+    LRURemove(e);
+    LRUAppend(&lru_, e);
+  }
+}
+
+void LRUCache::LRURemove(LRUHandle* e) {
+  e->next->prev = e->prev;
+  e->prev->next = e->next;
+}
+
+void LRUCache::LRUAppend(LRUHandle* list, LRUHandle* e) {
+  // Make "e" newest entry by inserting just before *list
+  e->next = list;
+  e->prev = list->prev;
+  e->prev->next = e;
+  e->next->prev = e;
+}
+
+Cache::Handle* LRUCache::Lookup(const std::string_view& key, uint32_t hash) {
+  std::lock_guard<BthreadMutex> l(mutex_);
+  LRUHandle* e = table_.Lookup(key, hash);
+  if (e != nullptr) {
+    Ref(e);
+  }
+  return reinterpret_cast<Cache::Handle*>(e);
+}
+
+void LRUCache::Release(Cache::Handle* handle) {
+  std::lock_guard<BthreadMutex> l(mutex_);
+  Unref(reinterpret_cast<LRUHandle*>(handle));
+}
+
+Cache::Handle* LRUCache::Insert(const std::string_view& key, uint32_t hash,
+                                void* value, size_t charge,
+                                void (*deleter)(const std::string_view& key,
+                                                void* value)) {
+  std::lock_guard<BthreadMutex> l(mutex_);
+
+  LRUHandle* e =
+      reinterpret_cast<LRUHandle*>(malloc(sizeof(LRUHandle) - 1 + key.size()));
+  e->value = value;
+  e->deleter = deleter;
+  e->charge = charge;
+  e->key_length = key.size();
+  e->hash = hash;
+  e->in_cache = false;
+  e->refs = 1;  // for the returned handle.
+  std::memcpy(e->key_data, key.data(), key.size());
+
+  if (capacity_ > 0) {
+    e->refs++;  // for the cache's reference.
+    e->in_cache = true;
+    LRUAppend(&in_use_, e);
+    usage_ += charge;
+    FinishErase(table_.Insert(e));
+  } else {  // don't cache. (capacity_==0 is supported and turns off caching.)
+    // next is read by key() in an assert, so it must be initialized
+    e->next = nullptr;
+  }
+
+  while (usage_ > capacity_ && lru_.next != &lru_) {
+    LRUHandle* old = lru_.next;
+    assert(old->refs == 1);
+    bool erased = FinishErase(table_.Remove(old->key(), old->hash));
+    if (!erased) {  // to avoid unused variable when compiled NDEBUG
+      assert(erased);
+    }
+  }
+
+  return reinterpret_cast<Cache::Handle*>(e);
+}
+
+// If e != nullptr, finish removing *e from the cache; it has already been
+// removed from the hash table.  Return whether e != nullptr.
+bool LRUCache::FinishErase(LRUHandle* e) {
+  if (e != nullptr) {
+    assert(e->in_cache);
+    LRURemove(e);
+    e->in_cache = false;
+    usage_ -= e->charge;
+    Unref(e);
+  }
+  return e != nullptr;
+}
+
+void LRUCache::Erase(const std::string_view& key, uint32_t hash) {
+  std::lock_guard<BthreadMutex> l(mutex_);
+  FinishErase(table_.Remove(key, hash));
+}
+
+void LRUCache::Prune() {
+  std::lock_guard<BthreadMutex> l(mutex_);
+  while (lru_.next != &lru_) {
+    LRUHandle* e = lru_.next;
+    assert(e->refs == 1);
+    bool erased = FinishErase(table_.Remove(e->key(), e->hash));
+    if (!erased) {  // to avoid unused variable when compiled NDEBUG
+      assert(erased);
+    }
+  }
+}
 
 BlockStatSPtr BlockMap::GetOrCreateBlockStat(const BlockKey& key) {
   uint64_t slice_id = key.id;
@@ -59,6 +226,67 @@ BlockStatSPtr BlockMap::GetOrCreateBlockStat(const BlockKey& key) {
   return *m.insert(slice_id, std::make_shared<BlockStat>());
 }
 
+class BlockPrefetcher::WaitingLobby {
+ public:
+  void AddWaiters(const std::vector<Waiter>& waiters) {
+    std::lock_guard<bthread::Mutex> lk(mutex_);
+    auto* waiters = waiters_.seek(waiter.key.ToString());
+    if (waiters == nullptr) {
+      waiters = new std::vector<Waiter>();
+      waiters_.insert(waiter.key.ToString(), waiters);
+    }
+
+    waiters->emplace_back(waiter);
+  }
+
+  std::vector<Waiter>* TakeWaiters(const std::string& key) {
+    std::lock_guard<bthread::Mutex> lk(mutex_);
+    auto it = waiters_.find(key);
+    if (it != waiters_.end()) {
+      auto* waiters = it->second;
+      waiters_.erase(it);
+      return waiters;
+    }
+
+    return nullptr;
+  }
+
+ private:
+  bthread::Mutex mutex_;
+  butil::FlatMap<std::string, std::vector<Waiter>*> waiters_;
+};
+
+class BlockPrefetcher::OnSegmentFetched : public Closure {
+ public:
+  OnSegmentFetched(IOBuffer* buffer, off_t offset, size_t length)
+      : buffer_(buffer), offset_(offset), length_(length) {}
+
+  void CopySegment(IOBuffer* in) { in->AppendTo(buffer_, length_, offset_); }
+
+  void Wait() {
+    std::unique_lock<bthread::Mutex> lk(mutex_);
+    while (!done_) {
+      cv_.wait(lk);
+    }
+  }
+
+  void Run() override {
+    {
+      std::lock_guard<bthread::Mutex> lk(mutex_);
+      done_ = true;
+    }
+    cv_.notify_one();
+  }
+
+ private:
+  IOBuffer* buffer_;
+  off_t offset_;
+  size_t length_;
+  bool done_;
+  bthread::Mutex mutex_;
+  bthread::ConditionVariable cv_;
+};
+
 void BlockPrefetcher::Start() {
   bthread::ExecutionQueueOptions options;
   options.use_pthread = true;
@@ -73,30 +301,89 @@ void BlockPrefetcher::Shutdown() {
   // bthread::execution_queue_join(queue_id_);
 }
 
+void BlockPrefetcher::SubmitTasks(const std::vector<Task>& tasks) {
+  //
+}
+
+void BlockPrefetcher::AddWaiters(const std::vector<Waiter>& waiters) {
+  waiting_lobby_->AddWaiters(
+      waiters);  // 这里会不会出现加进去之前已经完成了？造成死等？
+}
+
 int BlockPrefetcher::HandleTasks(
     void* meta, bthread::TaskIterator<std::vector<Task>*>& iter) {
   if (iter.is_queue_stopped()) {
     return 0;
   }
 
+  std::vector<Waiter> waiters;
+  std::vector<Task> tasks;
   auto* self = static_cast<BlockPrefetcher*>(meta);
   for (; iter; iter++) {
     auto* tasks = *iter;
+
     for (const auto& task : *tasks) {
-      self->HandleTask(task);
+      waiters.emplace_back(Waiter{
+          .key = task.key,
+          .segment_index = task.segment_index,
+          .done = task.done,
+      });
     }
+
+    // add tasks to tasks
+  }
+
+  self->waiting_lobby_->AddWaiters(waiters);
+  for (const auto& task : tasks) {
+    self->DoPrefetch(task);
   }
 
   return 0;
 }
 
-void BlockPrefetcher::HandleTask(const Task& task) {
-  auto segment_index = task.segment_index;
-  if (task.stat->SegmentExists(segment_index)) {
-  } else if (task.stat->SegmentPrefetching(segment_index)) {
+void BlockPrefetcher::DoPrefetch(const Task& task) {
+  auto& stat = task.stat;
+  for (;;) {
+    auto succ = stat->SetSegmentPrefetching(task.segment_index);
+    if (succ) {
+      break;
+    }
+
+    auto st = stat->GetSegmentStatus(task.segment_index);
+    if (st.IsExist()) {
+      return;
+    } else if (st.IsPrfetching()) {
+      //
+    }
   }
 
-  //
+  auto tid = RunInBthread([this, task]() {
+    auto* buffer = new IOBuffer();
+    auto status = remote_node_->Range(nullptr, task.key, task.offset,
+                                      task.length, &buffer);
+    if (status.ok()) {
+      auto* handler = cache_->Insert();
+      auto succ = stat->SetSegmentExist(task.segment_index);
+    } else {
+      LOG(WARNING) << "Prefetch block segment failed: " << status.ToString();
+    }
+
+    auto* waiters = waiting_lobby_->TakeWaiters(task.key.ToString());
+    if (waiters != nullptr) {
+      for (const auto& waiter : *waiters) {
+        CHECK_NOTNULL(waiter.done);
+        auto* closure = waiter.closure;
+        closure->CopySegment(handle->Value());
+        closure->status() = status;
+        closure->Run();
+      }
+      delete waiters;
+    }
+  });
+
+  if (tid != 0) {
+    joiner_->BackgroundJoin(tid);
+  }
 }
 
 CacheRetriever::CacheRetriever()
@@ -108,81 +395,115 @@ Status CacheRetriever::Range(const BlockKey& key, off_t offset, size_t length,
                              size_t total_length, IOBuffer* buffer) {
   CHECK_GT(total_length, 0);
 
+  SegmentHandler handler;
+  std::vector<SegmentHandler> handlers;
+  std::vector<BlockPrefetcher::Waiter> waiters;
+  std::vector<BlockPrefetcher::Task> to_prefetch;
   const auto& stat = block_map_->GetBlockStat(key);
 
   auto lindex = SegmentIndex(offset);
   auto rindex = SegmentIndex(offset + length);
   auto tindex = SegmentIndex(offset + total_length);
 
-  std::vector<off_t> caching;
-  std::vector<BlockPrefetcher::Task*> to_prefetch;
-  std::vector<SegmentHandler> handlers;
-
   for (off_t index = lindex; index <= tindex; ++index) {
-    bool exist = stat->SegmentExists(index);
-    bool prefetching = stat->SegmentPrefetching(index);
+    auto st = stat->GetSegmentStatus(index);
     bool care = (index <= rindex);
 
-    if (exist) {
+    if (st.IsExist()) {
       if (care) {
-        caching.push_back(index);
+        handler = CacheHandler();
+        handlers.emplace_back(std::move(handler));
         VLOG(3) << "Pick cache segment (" << index << ")";
       }
-      continue;
+    } else if (st.IsPrfetching()) {
+      if (care) {
+        auto waiter = BlockPrefetcher::Waiter();
+        waiter.key = key;
+        waiter.segment_index = index;
+        waiter.closure = NewSegmentClosure(index, buffer);
+        waiters.emplace_back(std::move(waiter));
+
+        handlers.emplace_back(WaitHandler(waiter.closure));
+      }
+      //
+    } else {  // not prefetching
+      auto task = BlockPrefetcher::Task();
+      task.key = key;
+      task.segment_index = index;
+      if (care) {
+        task.closure = NewSegmentClosure(index, buffer);
+        handlers.emplace_back(WaitHandler(task.closure));
+      }
+
+      to_prefetch.emplace_back(std::move(task));
     }
-
-    auto* task = new BlockPrefetcher::Task();
-    task->key = key;
-    task->segment_index = index;
-    to_prefetch.emplace_back(task);
-
-    // not exist
   }
 
-  // Submit prefetch task
-  prefetcher_->Submit(to_prefetch);
+  // not exist
 
+  // Submit prefetch task
+  prefetcher_->SubmitTasks(to_prefetch);
+
+  // Execute handler one by one
   for (auto& handler : handlers) {
     auto status = handler.Execute();
     if (!status.ok()) {
       status = handler.Retry();
+      if (!status.ok()) {
+        return status;
+      }
     }
   }
 
-  //
-
   return Status::OK();
+}
+
+BlockPrefetcher::OnSegmentFetched CacheRetriever::NewSegmentClosure(
+    IOBuffer* buffer, off_t offset, size_t length) {
+  return BlockPrefetcher::OnSegmentFetched(buffer, offset, length);
 }
 
 Status CacheHandler::Execute() {
   // 从 lrucache 里拿一下
+  auto* handler = cache_->Find();
+  if (handler == nullptr) {
+    return Status::NotFound("segment not cached");
+  }
+
+  auto* buffer = static_cast<IOBuffer*>(handler->Value());
+  out_->Append(buffer);
+
+  handler->Release();
   return Status::OK();
 }
 
 Status CacheHandler::Retry() {
+  auto status = storage_->Range();
   //
   return Status::OK();
 }
 
-Status PrefetchHandler::Execute() {
+Status WaitHandler::Execute() {
   CHECK_NOTNULL(task->done);
+
+  done_->Wait();
+  auto status = done_->status();
+  delete closure_;
 
   // 在这里 wait
   // task->done->wait();
   // 一旦这个 prefetch 任务完成，先放进 lrucache，再将 buffer 回调给所有 waiter
   // 防止一旦 buffer 加进去就被淘汰了
 
+  // return Status::OK();
+}
+
+Status WaitHandler::Retry() {
+  // 这里再去 retry 下 storage
+  auto status = storage_->Range(key, );
   return Status::OK();
 }
 
-Status PrefetchHandler::Retry() {
-  // 这里再去 retry 下 storage
-  storage_->Range(key, )
-
-      return Status::OK();
-}
-
 // BlockPrefetcher::BlockPrefetcher() {}
-
 };  // namespace cache
 };  // namespace dingofs
--- src/cache/remotecache/prefetch.h
+++ src/cache/remotecache/prefetch.h
@@ -34,12 +34,14 @@
 
 #include <atomic>
 #include <cstddef>
+#include <mutex>
 
 #include "cache/blockcache/cache_store.h"
 #include "cache/blockcache/lru_cache.h"
 #include "cache/remotecache/remote_cache_node.h"
 #include "cache/storage/closure.h"
 #include "cache/storage/storage.h"
+#include "cache/utils/bthread.h"
 #include "common/io_buffer.h"
 #include "common/status.h"
 
@@ -49,8 +51,129 @@ namespace cache {
 
 const constexpr size_t kSegmentSize = 128 * 1024;  // 128KB
 
-class LRUCache {
+class SegmentLRUCache {
  public:
+  // Cache() = default;
+
+  // Cache(const Cache&) = delete;
+  // Cache& operator=(const Cache&) = delete;
+
+  //// Destroys all existing entries by calling the "deleter"
+  //// function that was passed to the constructor.
+  // virtual ~Cache();
+
+  // Opaque handle to an entry stored in the cache.
+  struct Handle {};
+  class HandleTable;
+
+  virtual Handle* Insert(const std::string_view& key, void* value,
+                         size_t charge,
+                         void (*deleter)(const std::string_view& key,
+                                         void* value));
+
+  virtual Handle* Lookup(const std::string_view& key);
+
+  virtual void Release(Handle* handle);
+
+  virtual std::string_view Key(Handle* handle);
+
+  virtual void* Value(Handle* handle);
+
+  virtual void Erase(const std::string_view& key);
+
+  virtual uint64_t NewId();
+
+  virtual void Prune() {}
+
+  virtual size_t TotalCharge() const;
+
+ private:
+  void LRURemove(Handle* e);
+  void LRUAppend(Handle* list, Handle* e);
+
+  void Ref(Handle* e);
+  void Unref(Handle* e);
+
+  bool FinishErase(Handle* e);
+
+  mutable bthread::Mutex mutex_;
+  size_t capacity_;
+  size_t usage_;
+  Handle lru_;
+  Handle in_use_;
+  HandleTable table_;
+};
+
+class SharedSegmentCache : public SegmentCache {
+ public:
+  explicit SharedSegmentCache(size_t capacity) : last_id_(0) {
+    const size_t per_shard = (capacity + (kNumShards - 1)) / kNumShards;
+    for (auto& s : shard_) {
+      s.SetCapacity(per_shard);
+    }
+  }
+
+  ~SharedSegmentCache() = default;
+
+  Handle* Insert(const std::string_view& key, void* value, size_t charge,
+                 void (*deleter)(const std::string_view& key,
+                                 void* value)) override {
+    const uint32_t hash = HashSlice(key);
+    return shard_[Shard(hash)].Insert(key, hash, value, charge, deleter);
+  }
+
+  Handle* Lookup(const std::string_view& key) override {
+    const uint32_t hash = HashSlice(key);
+    return shard_[Shard(hash)].Lookup(key, hash);
+  }
+
+  void Release(Handle* handle) override {
+    LRUHandle* h = reinterpret_cast<LRUHandle*>(handle);
+    shard_[Shard(h->hash)].Release(handle);
+  }
+
+  void Erase(const std::string_view& key) override {
+    const uint32_t hash = HashSlice(key);
+    shard_[Shard(hash)].Erase(key, hash);
+  }
+
+  std::string_view Key(Handle* handle) override {
+    return reinterpret_cast<LRUHandle*>(handle)->key();
+  }
+
+  void* Value(Handle* handle) override {
+    return reinterpret_cast<LRUHandle*>(handle)->value;
+  }
+
+  uint64_t NewId() override {
+    std::lock_guard<BthreadMutex> l(id_mutex_);
+    return ++(last_id_);
+  }
+
+  void Prune() override {
+    for (auto& s : shard_) {
+      s.Prune();
+    }
+  }
+
+  size_t TotalCharge() const override {
+    size_t total = 0;
+    for (const auto& s : shard_) {
+      total += s.TotalCharge();
+    }
+    return total;
+  }
+
+ private:
+  static inline uint32_t HashSlice(const std::string_view& s) {
+    return Hash(s.data(), s.size(), 0);
+  }
+
+  static uint32_t Shard(uint32_t hash) { return hash >> (32 - kNumShardBits); }
+
+  LRUCache shard_[kNumShards];
+  BthreadMutex id_mutex_;
+  uint64_t last_id_;
 };
 
 using LRUCacheUPtr = std::unique_ptr<LRUCache>;
@@ -59,17 +182,38 @@ using LRUCacheSPtr = std::shared_ptr<LRUCache>;
 // block will be sliced into multiple segments, each segment is 128KB
 class BlockStat {
  public:
+  enum kStatus : uint8_t {
+    kNotExist = 0,
+    kPrefetching = 1,
+    kExist = 2,
+  };
+
+  struct SegmentStatus {
+    SegmentStatus(int st) : st(st) {}
+
+    bool IsPrfetching() const { return st == 1; }
+    bool IsExist() const { return st == 2; }
+
+    uint8_t st;
+  };
+
   BlockStat() = default;
+
+  SegmentStatus GetSegmentStatus(int index) {
+    auto st = st_[index % kSegmentNum].load(std::memory_order_relaxed);
+    return SegmentStatus(st);
+  }
 
   bool SegmentPrefetching(int index) {
     return st_[index % kSegmentNum].load(std::memory_order_relaxed) == 1;
   }
 
-  bool SegmentExists(int index) {
-    return st_[index % kSegmentNum].load(std::memory_order_relaxed) == 2;
-  }
+  bool SegmentExists(int index) { return == 2; }
 
   void SetSegmentPrefetching(int index) {
+    auto old_st = st_[index % kSegmentNum].compare_exchange_strong(
+        kNotExist, kPrefetching, std::memory_order_relaxed,
+        std::memory_order_relaxed);
     // TODO
   }
 
@@ -83,6 +227,10 @@ class BlockStat {
 
  private:
   static constexpr size_t kSegmentNum = 32;  // 4MB/128KB
+
+  int GetStatus(int index) {
+    return st_[index % kSegmentNum].load(std::memory_order_relaxed);
+  }
 
   // status:
   //   0: not exist
@@ -131,13 +279,21 @@ class SharedBlockMap : public BlockMap {
 
 class BlockPrefetcher {
  public:
+  class WaitingLobby;
+  class OnSegmentFetched;
+
   struct Task {
     BlockKey key;
-    off_t offset;
-    size_t length;
     int segment_index;
+    size_t total_length;
     BlockStatSPtr stat;
-    Closure* done;
+    OnSegmentFetched* closure;
+  };
+
+  struct Waiter {
+    BlockKey key;
+    int segment_index;
+    OnSegmentFetched* closure;
   };
 
   BlockPrefetcher();
@@ -145,16 +301,21 @@ class BlockPrefetcher {
   void Start();
   void Shutdown();
 
-  void Submit(const std::vector<Task>& tasks);
+  void SubmitTasks(const std::vector<Task>& tasks);
+  void AddWaiters(const std::vector<Waiter>& waiters);
 
  private:
   static int HandleTasks(void* meta,
                          bthread::TaskIterator<std::vector<Task>*>& iter);
-  void HandleTask(const Task& task);
 
+  void DoPrefetch(const Task& task);
+
+  bthread::RWLock rwlock_;
   LRUCacheSPtr cache_;
   RemoteCacheNodeSPtr remote_node_;
-  bthread::ExecutionQueue<std::vector<Task>*> queue_id_;
+  BthreadJoinerUPtr joiner_;
+  bthread::ExecutionQueue<std::vector<Task>*> task_queue_;
+  WaitingLobbyUPtr waiting_lobby_;
 };
 
 using BlockPrefetcherUPtr = std::unique_ptr<BlockPrefetcher>;
@@ -170,6 +331,14 @@ class CacheRetriever {
 
  private:
   off_t SegmentIndex(off_t offset) { return offset / kSegmentSize; }
+  off_t SegmentOffset(off_t offset) {}
+  size_t SegmentLength(off_t offset, size_t length) {
+    // return std::min() - std::max();
+
+    // return std::min(length, kSegmentSize - SegmentOffset(offset));
+  }
+
+  BlockPrefetcher::OnSegmentFetched NewSegmentClosure(segment_index, buffer);
 
   BlockMapUPtr block_map_;
   LRUCacheUPtr cache_;
@@ -177,7 +346,7 @@ class CacheRetriever {
   StorageSPtr storage_;
 };
 
-class SegmentHandler {
+class SegmentHandler : public Closure {
  public:
   virtual ~SegmentHandler() = default;
 
@@ -187,6 +356,7 @@ class SegmentHandler {
 
 class CacheHandler : public SegmentHandler {
  public:
+  CacheHandler();
   CacheHandler(LRUCache* cache);
 
   Status Execute() override;
@@ -200,15 +370,18 @@ class CacheHandler : public SegmentHandler {
   StorageUPtr storage_;
 };
 
-class PrefetchHandler : public SegmentHandler {
+class WaitHandler : public SegmentHandler {
  public:
+  explicit WaitHandler(Closure* closure);
+
   Status Execute() override;
   Status Retry() override;
 
  private:
   off_t offset_;
   size_t length_;
-  IOBuffer* buffer_;
+  IOBuffer* out_;
+  Closure* done_;
   BlockPrefetcher::Task* task;
   StorageUPtr storage_;
 };
